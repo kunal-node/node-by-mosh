e3b2ad5c1e42d291cdc72482a5a27362af444687
Kunal Pardeshi <kunalpardeshi77@gmail.com>
2021-11-09 01:30:24 +0530

    handling HTTP GET Requests

--- 1.express-restful-app/index.js
+++ 1.express-restful-app/index.js
@@ -2,13 +2,26 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3, 4]);
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((course) => course.id === +req.params.id);
+  if (!course) {
+    res.status(404).send("Course with given id was not found...");
+  }
+  res.send(course);
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
